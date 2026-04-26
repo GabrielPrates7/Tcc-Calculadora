9e15b9e6836d27058ef3a11e656688c54f212cd7
Gabriel Prates <gabrieldprates11@gmail.com>
2026-04-26 12:15:48 -0300

    Finalizando atualizações

---
+++ frontend/src/utils/formatters.ts
@@ -0,0 +1,8 @@
+export const formatarBRL = (valor: string | number) => {
+  return Number(valor).toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  });
+};
